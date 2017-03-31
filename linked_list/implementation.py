4630e10265548cf90d35564c62ba9984e7a3462c
Fadoua <baouab.fadoua@gmail.com>
2017-03-31 04:18:44 +0000

    Initial Commit

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,4 +1,5 @@
 from .interface import AbstractLinkedList
+from .node import Node
 
 
 class LinkedList(AbstractLinkedList):
@@ -7,34 +8,111 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        self.start = None
+        self.end = None 
+        
+        if elements:
+            for e in elements:
+                self.append(e)
 
-    def __str__(self):
-        pass
+
+    def __str__(self): 
+        inner = ', '.join(str(elem) for elem in self)
+        return '[{}]'.format(inner)
+        
 
     def __len__(self):
-        pass
+        return self.count()
 
     def __iter__(self):
-        pass
+        node = self.start
+        while node:
+            yield node.elem
+            node = node.next
+        raise StopIteration
 
     def __getitem__(self, index):
-        pass
+        if index >= len(self):
+            raise IndexError
+        for idx, element in enumerate(self):
+            if idx == index:
+                return element
+        
 
     def __add__(self, other):
-        pass
+        new_list = LinkedList(elem for elem in self)
+        for e in other:
+            new_list.append(e)
+        return new_list
 
     def __iadd__(self, other):
-        pass
+        for e in other:
+            self.append(e)
+        return self
 
     def __eq__(self, other):
-        pass
+        node_self = self.start #first node from the linkedlist
+        node_other = other.start
+       
+       #all elements are equal => same length
+       #self node and other node should be equal
+       #same_length = len(self) == len(other)
+       
+        while True:
+            if not node_self and not node_other:
+                return True
+            
+            if not bool(node_self) or not bool(node_other):
+                return False
+            
+            if node_self.elem != node_other.elem:
+                return False
+ 
+            node_self = node_self.next
+            node_other = node_other.next
+            
+    
+    def __ne__(self,other):
+        return not self.__eq__(other)
+
 
     def append(self, elem):
-        pass
+        if not self.start:
+            self.start = Node(elem)
+            self.end = self.start
+        else:
+            self.end.next = Node(elem) #next=None
+            self.end = self.end.next
+        
 
     def count(self):
-        pass
+        _count = 0
+        for i in self:
+            _count += 1
+        return _count
+            
 
     def pop(self, index=None):
-        pass
+        if len(self) == 0 or index >= len(self):
+            raise IndexError
+        if index is None:
+            index = len(self)-1
+        
+        # pop(0)
+        if index == 0:
+            print('index0')
+            elem = self.start.elem
+            self.start = self.start.next
+            return elem
+        
+        prev_node = None
+        node = self.start
+        i = 0
+        
+        while True:
+            if i == index:
+                prev_node.next = node.next
+                return node.elem
+            prev_node = node
+            node = node.next
+            i+=1
